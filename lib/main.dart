547cf20565e95dcc2c9ec7ae1cc4d52df0a6c0a7
Kalcio <matiasmunoz.me@gmail.com>
2022-06-08 01:35:24 -0400

    Primeros datos BD creados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,32 @@
 import 'package:flutter/material.dart';
 
+class Book implements Comparable {
+  final int id;
+  final String name;
+  final String autor;
+  final int year;
+
+  const Book({
+    required this.id,
+    required this.name,
+    required this.autor,
+    required this.year,
+  });
+
+  @override
+  int compareTo(covariant Book other) => other.id.compareTo(other.id);
+
+  @override
+  bool operator ==(covariant Book other) => id == other.id;
+
+  @override
+  int get hashCode => id.hashCode;
+
+  @override
+  String toString() =>
+      'Book, id = $id, name: $name, autor: $autor, year: $year';
+}
+
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
